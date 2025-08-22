83ebf229ff41a1d1577ec915cf80b641b964f932
Ahmadraza1123 <ahmadraza19397@gmail.com>
2025-08-22 14:17:31 +0500

    today changis

--- api/models.py
+++ api/models.py
@@ -1,8 +1,16 @@
+from email.policy import default
+
 from django.db import models
 
 
 class Task(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Process', 'Process'),
+        ('Completed', 'Completed'),
+    )
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     deccription = models.TextField(max_length=500)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from api.models import Task
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
 
+        # def update(self,*args,**kwargs):
 
-class Taskserializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
+
+# class Taskserializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Task
+#         fields = '__all__'
